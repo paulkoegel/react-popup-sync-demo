4d65b63417c46e6defac86207573acaa120f2040
Paul Kögel <paul@railslove.com>
2017-02-07 15:17:11 +0100

    Cleanup.

--- src/Popout.js
+++ src/Popout.js
@@ -2,13 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Popout extends Component {
-  constructor(props) {
-    console.log('popout', props);
-    super(props);
-  }
-
   render() {
-    console.log('prender', this.props);
     return (
       <h1>{this.props.counter}</h1>
     );
--- src/index.js
+++ src/index.js
@@ -8,32 +8,26 @@ const appState = {
   counter: 0
 };
 
-const popout = window.open('http://localhost:3000/popout.html', 'Popout Demo', 'width=350, height=250');
-console.log(popout);
+const renderPopout = () => {
+  ReactDOM.render(
+    <Popout counter={appState.counter} />,
+    popout.document.getElementById('popout-root')
+  );
+};
 
-const increaseCounter = () => {
-  appState.counter++;
+const renderApp = () => {
   ReactDOM.render(
     <App counter={appState.counter} increaseCounter={increaseCounter} />,
     document.getElementById('root')
   );
-
-  ReactDOM.render(
-    <Popout counter={appState.counter} />,
-    popout.document.getElementById('popout-root')
-  );
 };
 
-ReactDOM.render(
-  <App counter={appState.counter} increaseCounter={increaseCounter} />,
-  document.getElementById('root')
-);
-
-console.log('popout container', popout, popout.document, popout.document.getElementById('popout-root'));
-
-popout.onload = () => {
-  ReactDOM.render(
-    <Popout counter={appState.counter} />,
-    popout.document.getElementById('popout-root')
-  );
+const increaseCounter = () => {
+  appState.counter++;
+  renderApp();
+  renderPopout();
 };
+
+renderApp();
+const popout = window.open('http://localhost:3000/popout.html', 'Popout Demo', 'width=350, height=250');
+popout.onload = renderPopout;
