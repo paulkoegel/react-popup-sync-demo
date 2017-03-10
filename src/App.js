e5f6941f530f4442548d0968726bf2ea674cd03d
Paul Kögel <paul@railslove.com>
2017-03-10 17:29:36 +0100

    Add users section.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,34 @@ import { connect } from 'react-redux';
 import './App.css';
 import { incrementCounter } from './actions';
 
-const App = ({ counter, onIncrease }) => (
-  <div>
-    <h1>App</h1>
-    <button onClick={onIncrease}>
-      {counter}
-    </button>
+const App = ({ counter, onIncrease, users}) => (
+  <div className='appWrapper'>
+    <section className='usersSection'>
+      <ol>
+        {users.map(u => (
+          <li key={u.id}>{u.name}</li>
+        ))}
+      </ol>
+    </section>
+
+    <section className='counterSection'>
+      <button onClick={onIncrease}>
+        {counter}
+      </button>
+    </section>
   </div>
 );
 
 const mapStateToProps = (state, ownProps) => (
   {
-    counter: state.counter
+    counter: state.counter,
+    users: state.users
   }
 );
 
 const mapDispatchToProps = (dispatch, ownProps) => (
   {
     onIncrease: () => {
-      console.log('onIncrease');
-      console.log(incrementCounter());
       dispatch(incrementCounter());
     }
   }
