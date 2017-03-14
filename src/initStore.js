fc20986899bf6bac5876e2c78608bf66a9c06ddf
Paul Kögel <paul@railslove.com>
2017-03-14 13:34:31 +0100

    doesn't break!?

--- src/initStore.js
+++ src/initStore.js
@@ -20,34 +20,43 @@ const store = createStore(reducer,
 
 store.subscribe(throttle(() => {
   // console.log('Redux store subscribe: set counter value in localstorage', store.getState().counter, JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY)));
-  // const persistedState = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY));
-  // const persistedCounter = persistedState && persistedState.counter;
-  // console.log("CHECK (persisted|state):", persistedCounter, store.getState().counter)
-  // if (store.getState().counter !== persistedCounter) {
+  const persistedState = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+  const persistedCounter = persistedState.counter;
+  console.log('REDUX STORE SUBSCRIBE (persisted|state):', persistedCounter, store.getState().counter);
+  if (store.getState().counter !== persistedCounter) {
+    console.log('REDUX STORE SUBSCRIBE: IF');
     // console.log('updating localstorage (before|after):', persistedCounter, store.getState().counter);
-    console.log('Redux store subscribe');
     window.localStorage.setItem(
       LOCAL_STORAGE_KEY,
       JSON.stringify({counter: store.getState().counter})
     );
-  // }
+  } else {
+    console.log('REDUX STORE SUBSCRIBE: ELSE');
+  }
 }),
 200);
 
+let theCounter = 0;
+
 // this listener will only be triggered in OTHER windows
 // it won't be triggered in the window where localStorage.setItem was called
-// In Firefox this listener is also not triggered when the values in localstorage
-// didn't change, preventing an infinite loop between the main and popup window.
+// In Firefox (not so in IE11), this listener is also not triggered when the values in
+// localstorage didn't change, which prevents an infinite loop between the main and
+// popup window.
 window.addEventListener('storage', (e) => {
   const newValue = JSON.parse(e.newValue) || {};
   const oldValue = JSON.parse(e.oldValue) || {};
 
-  console.log('storage (new|old)', newValue.counter, oldValue.counter);
-  if (newValue.counter !== oldValue.counter) {
-    console.log('IF, dispatch SET_COUNTER');
+  console.log('LS::EL (new|old):', newValue.counter, oldValue.counter);
+  // for IE11 we need this IF because the callback is triggered even when
+  // the store's state didn't change. It prevents an infinite loop between the
+  // popup and main window.
+  if (newValue.counter !== oldValue.counter && theCounter < 100) {
+    theCounter++;
+    console.log('LS::EL: IF, dispatch SET_COUNTER', theCounter);
     store.dispatch(setCounter(newValue.counter));
   } else {
-    console.log('ELSE');
+    console.log('LS::EL: ELSE', theCounter);
   }
 });
 
